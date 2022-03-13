979050446b2195d4e71ba7c7d4aa320cc8cadc32
tasnuva-zaman <tasnuva.zaman@sslwireless.com>
2022-03-13 11:21:45 +0600

    frontend for django api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import  {useState} from "react";
 
 function App() {
+
+  const [articles, setArticles] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Django and React </h3>
+
+      {articles.map(article => {
+        return <h2>{article}</h2>
+      })}
     </div>
   );
 }
